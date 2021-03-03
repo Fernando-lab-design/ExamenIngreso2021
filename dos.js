ac8c7ea51c55e5d21ce7f49913470fc2a78a3b6a
fernando-lab-design <dipaccefer@gmail.com>
2021-03-03 12:40:43 +0100

    solucionar problemas

--- dos.js
+++ dos.js
@@ -1,4 +1,113 @@
 function mostrar()
 {
-  alert("dos");
+
+  let nombre;
+  let cursada;
+  let materias;
+  let sexo;
+  let nota;
+  let edad;
+  let mejorProm;
+  let promMax;
+  let edadMenor;
+  let nombreMenor;
+  let acumNotaFem = 0;
+  let contNotaFem = 0;
+  let acumNotaMasc = 0;
+  let contNotaMasc = 0;
+  let acumNotaNB = 0;
+  let contNotaNB = 0;
+  let promFem;
+  let promMasc;
+  let promNB;
+  let maxMat;
+  let maxMatNomb;
+  let maxMatEdad;
+  let flagEdad = 1;
+  let flagMat = 1;
+  let flagSexo = 1;
+  let sigue;
+
+  do
+  {
+    nombre = prompt("Ingrese nombre: ");
+    cursada = prompt("Ingrese modalidad de cursada: libre/remota/presencial ");
+    while(cursada != "libre" && cursada != "presencial" && cursada != "remota")
+    {
+      cursada = prompt("Error. Ingrese modalidad de cursada: libre/remota/presencial ");
+    }
+    materias = parseInt(prompt("Ingrese cantidad de materias: "));
+    while(isNaN(materias) || materias < 0 || materias >= 8)
+    {
+      materias = parseInt(prompt("Error. Ingrese cantidad de materias: "));
+    }
+    sexo = prompt("Ingrese sexo: femenino/masculino/no binario: ");
+    while(sexo != "femenino" && sexo != "masculino" && sexo != "no binario")
+    {
+      sexo = prompt("No valido. Ingrese sexo: femenino/masculino/no binario: ");
+    }
+    nota = parseInt(prompt("Ingrese nota promedio:"));
+    while(isNaN(nota) || nota < 0 || nota >10)
+    {
+      nota = parseInt(prompt("No valido. Ingrese nota promedio:"));
+    }
+    edad = parseInt(prompt("Ingrese edad: "));
+    while(isNaN(edad) || edad < 0 )
+    {
+      edad = parseInt(prompt("no es valido. Ingrese edad: "));
+    }
+    if(sexo != "masculino")
+    {
+      if(flagSexo || nota > promMax)
+      {
+        promMax = nota;
+        mejorProm = nombre;
+        flagSexo = 0;
+      }
+    }
+    if(cursada == "libre")
+    {
+      if(flagEdad || edad < edadMenor)
+      edadMenor = edad;
+      nombreMenor = nombre;
+      flagEdad = 0;
+    }
+    switch(sexo)
+    {
+      case "femenino":
+        acumNotaFem += nota;
+        contNotaFem++;
+        break;
+      case "masculino":
+        acumNotaMasc += nota;
+        contNotaMasc++;
+        break;
+      case "no binario":
+        acumNotaNB += nota;
+        contNotaNB++;
+        break;
+    }
+    if(cursada != "remota")
+    {
+      if(flagMat || materias > maxMat)
+      {
+        maxMat = materias;
+        maxMatEdad = edad;
+        maxMatNomb = nombre;
+        flagMat = 0;
+      }
+    }
+    sigue = prompt("Desea ingresar otro alumno?", "si");
+  }while(sigue == "si");
+
+  promFem = acumNotaFem/contNotaFem;
+  promMasc = acumNotaMasc/contNotaMasc;
+  promNB = acumNotaNB/contNotaNB;
+  
+  document.write("a- El nombre del mejor promedio que no es masculino es: " + mejorProm + "<br>");
+  document.write("b- Entre los que dan libre el mas joven es: " + nombreMenor + "<br>");
+  document.write("c- Los promedio de nota por sexo son: femenino: " + promFem + "<br>" );
+  document.write(" masculino: " + promMasc + "<br>" );
+  document.write(" no binario: " + promNB + "<br>" );
+  document.write("d- El alumno que mas materias cursa que no es remota es " + maxMatNomb + " y su edad es " + maxMatEdad);
 }
--- uno.js
+++ uno.js
@@ -1,5 +1,141 @@
 
 function mostrar()
 {
-	alert("uno");
+	let producto;
+	let precio;
+	let unidades;
+	let tipo;
+	let marca;
+	let contAlc = 0;
+	let acumAlc = 0;
+	let contIac = 0;
+	let acumIac = 0;
+	let contQuat = 0;
+	let acumQuat = 0;
+	let promCantAlc;
+	let promCantIac;
+	let promCantQuat;
+	let flag = 1;
+	let tipoMax;
+	let acumIacBarato = 0;
+	let precioMax = 0;
+	let marcaCaro;
+	let tipoCaro;
+	let productoCaro;
+	let acumExpl =0;
+	let acumCom = 0;
+	let acumIgn =0;
+	
+
+
+	for(let i=0;i<5; i++)
+	{
+		producto = prompt("Ingrese el producto: ALCOHOL/IAC/QUAT").toUpperCase();
+		while(producto != "ALCOHOL" && producto != "IAC" && producto != "QUAT")
+		{
+			producto = prompt("No es valido. Ingrese el producto: ALCOHOL/IAC/QUAT").toUpperCase();
+		}
+		precio = parseFloat(prompt("Ingrese precio del producto: "));
+		while(isNaN(precio) || precio < 100 || precio >300)
+		{
+			precio = parseFloat(prompt("Valor incorrecto Ingrese precio del producto entre 100 y 300: "));
+		}
+		unidades = parseInt(prompt("Ingrese cantidad de unidades: "));
+		while(isNaN(unidades) || unidades <= 0 || unidades > 1000)
+		{
+			unidades = parseInt(prompt("No es valido. Ingrese cantidad de unidades: "));
+		}
+		tipo = prompt("Ingrese el tipo de inflamable: ignifugo/combustible/explosivo").toLowerCase();
+		while(tipo != "ignifugo" && tipo != "combustible" && tipo != "explosivo")
+		{
+			tipo = prompt("Error. Ingrese el tipo de inflamable: ignifugo/combustible/explosivo").toLowerCase();
+		}
+		marca = prompt("Ingrese marca del producto: ");
+		
+		switch(producto)
+		{
+			case "ALCOHOL":
+				contAlc++;
+				acumAlc += unidades;
+				break;
+			case "IAC":
+				contIac++
+				acumIac += unidades;
+				break;
+			case "QUAT":
+				contQuat++
+				acumQuat += unidades;
+				break;
+		}
+		switch(tipo)
+		{
+			case "ignifugo":
+				acumIgn += unidades;
+				break;
+			case "combustible":
+				acumCom += unidades;
+				break;
+			case "explosivo":
+				acumExpl += unidades;
+				break;
+		}
+		if(acumIgn > acumCom && acumIgn > acumExpl)
+		{
+			tipoMax = "ignifugo";
+		}
+		else if(acumCom >= acumIgn && acumCom > acumExpl)
+		{
+			tipoMax = "combustible";
+		}
+		else
+		{
+			tipoMax = "explosivo";
+		}
+		if(producto == "IAC" && precio <=200)
+		{
+			acumIacBarato += unidades;
+		}
+		if(flag || precio > precioMax)
+		{
+			precioMax = precio;
+			marcaCaro = marca;
+			tipoCaro = tipo;
+			productoCaro = producto;
+			flag = 0;
+		}
+		
+	}
+	
+	if(contAlc != 0)
+	{
+		promCantAlc = acumAlc / contAlc;
+		document.write("a- El promedio de cantidad de ALCOHOL es: " + promCantAlc + "<br>");
+	}
+	else
+	{
+		document.write("a- No se compraron unidades de ALCOHOL <br>");
+	}
+	if(contIac != 0)
+	{
+		promCantIac = acumIac/contIac;
+		document.write("a- El promedio de cantidad de IAC es: " + promCantIac + "<br>");
+	}
+	else
+	{
+		document.write("a- No se compraron unidades de IAC <br>");
+	}
+	if(contQuat != 0)
+	{
+		promCantQuat = acumQuat/contQuat;
+		document.write("a- El promedio de cantidad de QUAT es: " + promCantQuat + "<br>");
+	}
+	else 
+	{
+		document.write("a- No se compraron unidades de QUAT");
+	}
+	document.write("b- El tipo de inflamable con mayor unidades compradas es: " + tipoMax + "<br>");
+	document.write("c- Se compraron en total " + acumIacBarato + " unidades de IAC con precio menor a 200 <br>");
+	document.write("d- El producto mas caro es " + productoCaro + ", la marca es " + marcaCaro + " y el tipo de inflamable es " + tipoCaro + "<br>");
+	
+
 }
